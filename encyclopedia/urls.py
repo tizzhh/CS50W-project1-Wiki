38f5328645821b246c0d8c1f08e1e5963d0bcc96
tizzhh <darovadraste2@gmail.com>
2022-08-22 18:16:40 +0300

    Search, new page, edit page, random page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:name>", views.entry, name="entry"),
-    path("wiki", views.index, name="list")
+    path("wiki", views.index, name="list"),
+    path("search/", views.search, name="search"),
+    path("newpage", views.newpage, name="newpage"),
+    path("checkentry", views.checkentry, name="checkentry"),
+    path("editpage/<str:name>", views.editpage, name="editpage"),
+    path("saveentry", views.saveentry, name="saveentry"),
+    path("randompage", views.randompage, name="randompage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
-import re
+from audioop import reverse
+from html import entities
+import re, os.path, random
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 
 from . import util
 
@@ -14,5 +16,62 @@ def entry(request, name):
     entry = util.get_entry(name)
     return render(request, "encyclopedia/entry.html", {
         "entry": entry,
-        "name": name.capitalize()
+        "name": name
     })
+
+def search(request):
+    # https://stackoverflow.com/questions/53920004/add-q-searchterm-in-django-url
+    name = request.GET.get('q')
+    entry = util.get_entry(name)
+    if entry == None:
+        match = []
+        entries = util.list_entries()
+        for entry in entries:
+            if name.lower() in entry.lower():
+                match.append(entry)
+        return render(request, "encyclopedia/search_results.html", {
+            "entries": match
+        })
+    '''entries = util.list_entries()
+    for entry in entries:
+        if name in entry:
+            name = entry'''
+    return HttpResponseRedirect(f"../wiki/{name}")
+
+    '''entry = util.get_entry(name)
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "name": name.capitalize()
+    })'''
+
+def newpage(request):
+    return render(request, "encyclopedia/newpage.html")
+
+def checkentry(request):
+    title = request.GET.get('title')
+    entry = request.GET.get('entry')
+    # https://www.pythontutorial.net/python-basics/python-check-if-file-exists/
+    if os.path.exists(f"entries/{title}.md"):
+        return render(request, "encyclopedia/newpage.html", {
+            "check": True
+        })
+    with open(f"entries/{title}.md", "w") as file:
+        file.write(entry)
+    return HttpResponseRedirect(f"../wiki/{title}")
+
+def editpage(request, name):
+    return render(request, "encyclopedia/edit_page.html", {
+        "name": name,
+        "entry": util.get_entry(name)
+    })
+
+def saveentry(request):
+    title = request.GET.get('title')
+    entry = request.GET.get('entry')
+    with open(f"entries/{title}.md", "w") as file:
+        file.write(entry)
+    return HttpResponseRedirect(f"../wiki/{title}")
+
+def randompage(request):
+    entries = util.list_entries()
+    return HttpResponseRedirect(f"../wiki/{random.choice(entries)}")
